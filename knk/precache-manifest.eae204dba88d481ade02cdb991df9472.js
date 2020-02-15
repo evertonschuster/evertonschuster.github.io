a5b4e32969034e89d67730f45f8884fc0e058cf2
everton <everton.schuster98@gmail.com>
2020-02-15 20:21:24 -0300

    Nao tem

---
+++ knk/precache-manifest.eae204dba88d481ade02cdb991df9472.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db02af72445ac5212e649b617c974278",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23d95b697b0941f78afe",
+    "url": "/static/css/2.de915d7b.chunk.css"
+  },
+  {
+    "revision": "776ae351c63aa07c2934",
+    "url": "/static/css/main.c5c0920b.chunk.css"
+  },
+  {
+    "revision": "23d95b697b0941f78afe",
+    "url": "/static/js/2.5bbd97b3.chunk.js"
+  },
+  {
+    "revision": "776ae351c63aa07c2934",
+    "url": "/static/js/main.9dff857f.chunk.js"
+  },
+  {
+    "revision": "486c309c3fc942a9a773",
+    "url": "/static/js/runtime-main.0abccf36.js"
+  },
+  {
+    "revision": "1520f43432fc67cada28df5f9669ef5c",
+    "url": "/static/media/index.1520f434.less"
+  },
+  {
+    "revision": "2dc5f250598fc910d2b30ac047bc392e",
+    "url": "/static/media/index.2dc5f250.less"
+  },
+  {
+    "revision": "c17ec543e0d6a592928535f43867f969",
+    "url": "/static/media/index.c17ec543.less"
+  }
+]);
