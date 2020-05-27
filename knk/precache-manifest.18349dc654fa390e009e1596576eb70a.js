4fb181d175466c1316748c8ae721ec87f3683d68
everton <everton.schuster98@gmail.com>
2020-05-27 13:24:45 -0300

    In

---
+++ knk/precache-manifest.18349dc654fa390e009e1596576eb70a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d94418b3ae6d0d3b4a6f61d0e8665005",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4afe3cd19b8da0290ca2",
+    "url": "/static/css/2.3fa10eb0.chunk.css"
+  },
+  {
+    "revision": "96c37139f2b5f6d0a263",
+    "url": "/static/css/main.80934f83.chunk.css"
+  },
+  {
+    "revision": "4afe3cd19b8da0290ca2",
+    "url": "/static/js/2.df00ace0.chunk.js"
+  },
+  {
+    "revision": "33bd471150419da43ba923e2766dab5e",
+    "url": "/static/js/2.df00ace0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "96c37139f2b5f6d0a263",
+    "url": "/static/js/main.a7b5d75f.chunk.js"
+  },
+  {
+    "revision": "cae786d627993da55aef",
+    "url": "/static/js/runtime-main.a01297b7.js"
+  }
+]);
